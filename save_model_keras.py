3d9cd3b17e236f851c8ed40fcdd3e3e383ba065b
breadbread1984 <breadbread1984@163.com>
2021-04-02 22:30:51 +0800

    update

---
+++ save_model_keras.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import tensorflow as tf;
+from models import YOLOv4;
+
+def main():
+
+  yolov4 = YOLOv4((416, 416, 3), 80);
+  yolov4.load_weights('./checkpoints/ckpt/variables/variables');
+  yolov4.save('yolov4.h5');
+  yolov4.save_weights('yolov4_weights.h5');
+
+if __name__ == "__main__":
+
+  assert tf.executing_eagerly();
+  main();
+
